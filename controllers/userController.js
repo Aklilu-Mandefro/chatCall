593b8ad372bf7ac18f3ba818ce25b2a21e9e10c5
wanzbrayyy <wanzbray@gmail.com>
2025-08-03 04:38:38 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,26 +17,25 @@ const signToken = id => {
 }
 
 const createSendToken = (user, statusCode, res, msg) => {
-    const token = signToken(user._id);
-    const cookieOptions = {
-        expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
-    };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-    res.cookie('jwt', token, cookieOptions);
-    res.cookie('user_id', user.id, cookieOptions);
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production'
+  };
 
-    // Remove password from output
-    user.password = undefined;
-    return res.status(statusCode).json({
-        status: 'success',
-        message: msg,
-        token,
-        data: {user}
-    });
-}
+  res.cookie('jwt', token, cookieOptions);
+  res.cookie('user_id', user.id, cookieOptions);
+
+  user.password = undefined;
+
+  return res.status(statusCode).json({
+    status: 'success',
+    message: msg,
+    token,
+    data: { user }
+  });
+};
 
 /**
  * Sign Up
